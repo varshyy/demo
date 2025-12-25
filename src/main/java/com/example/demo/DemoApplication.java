576e58b5ea8752468b21efcb7b67ea841495a8f2
nvarshini2002 <nvarshini2002@gmail.com>
2025-12-25 05:09:49 +0000

    First Commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,7 +16,7 @@ public class DemoApplication {
   class HelloworldController {
     @GetMapping("/")
     String hello() {
-      return "Hello " + name + "!";
+      return "Hello Varshini" + name + "!";
     }
   }
 
